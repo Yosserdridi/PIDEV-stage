ad1c85f18c61e3aeb23fd9bde6fed121f0fcef83
hichr00khalil <hichrikhalilo06@gmail.com>
2025-02-15 17:05:38 +0100

    enetits

---
+++ Back/src/main/java/com/example/back/entities/TypeInternship.java
@@ -0,0 +1,10 @@
+package com.example.back.entities;
+
+public enum TypeInternship {
+
+        INTERNSHIP_PFE,
+        INTERNSHIP_FORMATION_HUMAINE_ET_SOCIALE,
+        INTERNSHIP_D_IMMERSION_EN_ENTREPRISE,
+        INTERNSHIP_INGENIEUR
+    }
+
---
+++ Back/src/main/java/com/example/back/entities/User.java
@@ -0,0 +1,34 @@
+package com.example.back.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Inheritance(strategy = InheritanceType.JOINED)
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String lastName;
+    private String firstName;
+    private String email;
+    private String password;
+    private Boolean isAdmin ;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private Set<Post> posts ;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private Set<Complaint> complaints ;
+}
